cca77bb90a6930189169532fca675436df39f6b4
Surik95 <pokemon.surik@gmail.com>
2022-12-22 11:05:21 +0300

    выполнил задание

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import Listing from "./components/Listing";
+import "./App.css";
+import etsy from "./data/etsy.json";
+
+// const arrItems = JSON.parse(etsy);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Listing items={etsy} />;
 }
 
 export default App;
